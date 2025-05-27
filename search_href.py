bea0138342337ef8393db0a6acfa8739103ced78
Guojian <aaa@lfcypo.com>
2025-05-27 14:01:17 +0800

    feat: 增加 first 参数遍历功能

--- search_href.py
+++ search_href.py
@@ -13,25 +13,26 @@ def get_headers():
 
 def get_url():
     searchWeb = "https://cn.bing.com/search?q={}"
-    searchName = parse.quote(input("请输入想要搜索的东西: "))
-    searchUrl = searchWeb.format(searchName)
+    searchFinding = str(input("请输入想要搜索的东西: "))
+    if searchFinding == "":
+        print("请输入有效查询目标")
+        return get_url()
+    searchUrl = searchWeb.format(parse.quote(searchFinding))
     return searchUrl
 
 
-def get_html():
-    response = requests.get(url=get_url(), headers=get_headers())
+def get_html(url, headers):
+    response = requests.get(url, headers)
     if response.status_code == 200:
         response.encoding = "utf-8"
         html = response.text
         return html
     else:
         print("请求失败")
-        return "None"
+        return None
 
 
-total = 0
-def search_href():
-    html = get_html()
+def search_href(html):
     soup = BeautifulSoup(html,"lxml")
     with open("search_href.txt", "w", encoding="utf-8") as f:
         for i in soup.find_all("a",attrs={"target":"_blank"}):
@@ -47,6 +48,9 @@ def search_href():
         f.close()
 
 
+def search_website(url, amount):
+    pass
+
+
 if __name__ == "__main__":
-    search_href()
     print("OK")
