a251d14cdc3bc070b0e2eb8b1c0d1ebbcef029c4
GaxCal <489771132@qq.com>
2025-05-29 14:04:58 +0800

    feat: 修改 first 功能

--- search_href.py
+++ search_href.py
@@ -1,17 +1,18 @@
 import requests
 from urllib import parse
 from bs4 import BeautifulSoup
+from typing import Optional
 from fake_useragent import UserAgent
 
 
-def get_headers():
+def get_headers() -> dict:
     ua = UserAgent(platforms=['desktop'], browsers=['Edge']).random
-    headers = {'UserAgent': ua}
+    headers = {'User-Agent': ua}
     print("随机获得的请求头部",headers)
     return headers
 
 
-def get_url():
+def get_url() -> str:
     searchWeb = 'https://cn.bing.com/search?q={}'
     searchFinding = str(input("请输入想要搜索的东西: "))
     if searchFinding == '':
@@ -21,36 +22,33 @@ def get_url():
     return searchUrl
 
 
-def get_html(url, headers):
+def search_href(url, headers) -> Optional[list]:
+    global total, websiteList
+    if headers is None:
+        headers = get_headers()
     response = requests.get(url, headers)
     if response.status_code == 200:
         response.encoding = 'utf-8'
         html = response.text
-        return html
     else:
         print("请求失败")
         return None
-
-
-def search_href(html):
     soup = BeautifulSoup(html,"lxml")
-    with open('search_href.txt', 'w', encoding='utf-8') as f:
-        for i in soup.find_all('a',attrs={'target':'_blank'}):
-            href = i.get('href')
-            text = i.get_text()
-            if href is None:
-                continue
-            else:
-                f.write(href)
-                f.write('     ')
-                f.write(text)
-                f.write('\n')
-        f.close()
+    websiteFind = soup.find_all('a',attrs={'target':'_blank'})
+    total = total + len(websiteFind)
+    for i in websiteFind:
+        websiteList.append({i.get('href'),i.get_text()})
+    return websiteList
 
 
-def search_website(url, amount):
-    pass
+def search_website(searchUrl, amount):
+    if total < amount:
+        newUrl = (searchUrl + "first{}").format(str(total+1))
+        search_href(newUrl, get_headers())
+        return search_website(searchUrl, amount)
 
 
 if __name__ == '__main__':
+    total = 0
+    websiteList = []
     print("OK")
